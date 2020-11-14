715bbbabe1652bd06cc78a3dbe2bc43356a4eb70
Kah Ho <sweekahho@gmail.com>
2020-11-14 21:02:15 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Sentry-Project Website
+NodeJS website for Portfolio 2 project, Sentry.
+Main project can be found [here](https://github.com/kahkoii/Sentry-Project).
+
+# Hosted on Heroku
+Live website can be found [here](https://sentry-p2.herokuapp.com/).
+
+# Running the app locally
+The app can be run using **nodemon run** or **node app**
+After running, the website can be found on **localhost:3000**
---
+++ app.js
@@ -0,0 +1,20 @@
+// Packages
+var express = require('express');
+var app = express();
+
+// Port
+var PORT = process.env.PORT || 3000;
+app.listen(PORT);
+
+// Define templating engine
+app.set('view engine', 'ejs');
+
+// Use static files
+app.use(express.static('./public'));
+
+// Routing
+var student = require('./controllers/student');
+var staff = require('./controllers/staff');
+app.use('/', student);
+app.use('/student', student);
+app.use('/staff', staff);
