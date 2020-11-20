31d80315c145f754f3067c1c6db81189bc927163
Kah Ho <sweekahho@gmail.com>
2020-11-20 10:46:06 +0800

    Adds logic to pass login ID to server

--- controllers/student.js
+++ controllers/student.js
@@ -4,12 +4,15 @@ var bodyParser = require('body-parser');
 var parser = bodyParser.urlencoded({ extended: false, limit: '10mb' });
 var fs = require('fs');
 var dal = require('./DAL')
+var studentID = 'S12345678A';
 
 router.get('/', function (req, res){
     res.render('login', { title: 'Login'});
 })
 
-router.post('/', function(req, res){
+router.post('/', parser, function(req, res){
+    studentID = req.body.studentID;
+    console.log(studentID);
     res.render('studentHome', { title: 'Home'})
 })
 
@@ -18,14 +21,13 @@ router.get('/upload', function (req, res){
 })
 
 router.post('/upload', parser, function (req, res){
-    var studentNo = 'S12345678A';
     var image = req.body.image.replace(/^data:image\/png;base64,/, "");
     fs.writeFile("./temp/out.png", image, 'base64', function(err) {
         if (err != null){
             console.log(err);
         }
     });
-    dal.upload(studentNo);
+    dal.upload(studentID);
     res.render('success', { title: 'Success'});
 })
 
