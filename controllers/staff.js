6a6d0b1b49e91ec22e2401de2fe6c5632db9a266
noderous <s10185333@connect.np.edu.sg>
2021-01-26 13:40:06 +0800

    Displayed DB data to views

--- controllers/staff.js
+++ controllers/staff.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+/*import package to connect to db*/
+var MongoClient = require('mongodb').MongoClient;
+/*connection string to connect to db */
+var url = "mongodb+srv://dbUser:Asteria987@cluster0.plmvw.mongodb.net/Cluster0?retryWrites=true&w=majority";
+
 router.get('/', function (req, res){
-    res.render('staffHome');
+    //----get data from DB----//
+    /*method to retrieve collection*/
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err; // if error occure
+        var dbo = db.db("Sentry"); // name of db
+        dbo.collection("Logs").find({}).toArray(function(err, result) {
+            if (err) throw err; // if error occure
+            var r = result; // type is object
+            db.close(); // close db
+            res.render('staffHome', {title: 'Home', items: r});// sends data to web page
+        });
+    });
 })
 
 router.get('/statistics', function (req, res){
