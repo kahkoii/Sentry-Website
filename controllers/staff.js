29646f535b7934cbe64e02a42dcea99ac76fc040
Kah Ho <sweekahho@gmail.com>
2021-01-26 12:08:50 +0800

    Create basic staff view

--- controllers/staff.js
+++ controllers/staff.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res){
-    res.render('staffHome', { title: 'Home' });
+    res.render('staffHome');
+})
+
+router.get('/statistics', function (req, res){
+    res.render('staffStatistics');
 })
 
 module.exports = router;
