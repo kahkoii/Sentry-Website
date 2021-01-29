3c8d842448b2a4b8702669277489c33f513c6e19
noderous <s10185333@connect.np.edu.sg>
2021-01-29 10:55:46 +0800

    Update DAL code to append image data to existing id entries

--- controllers/DAL.js
+++ controllers/DAL.js
@@ -1,20 +1,34 @@
 function uploadImg(studentNo, image){
     const fs = require('fs');
-    const MongoClient = require('mongodb').MongoClient;
-    const url = "mongodb+srv://dbUser:Asteria987@cluster0.plmvw.mongodb.net/Cluster0?retryWrites=true&w=majority";
+    var MongoClient = require('mongodb').MongoClient;
+    var url = "mongodb+srv://dbUser:Asteria987@cluster0.plmvw.mongodb.net/Cluster0?retryWrites=true&w=majority";
 
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        var dbo = db.db("Sentry");
-        // Json object
-        var myobj = { "_id": studentNo, "name": "oH haK eewS", "Images": [image]};
-
-        dbo.collection("Images").insertOne(myobj, function(err, res) {
-          if (err) throw err;
-          console.log("1 document inserted");
-          db.close();
-        });
-    });
+    const client = new MongoClient(url);
+    async function run() {
+      try {
+        await client.connect();
+        const database = client.db("Sentry");
+        const collection = database.collection("Images");
+        const filter = { "_id": studentNo };
+        const options = { upsert: true };
+        const updateDoc = {
+          $push: {
+            "Images": image
+          }
+        };
+        
+        const result = await collection.updateOne(filter, updateDoc, options);
+        if (result.matchedCount == 0)
+          console.log("A document is created");
+        else
+          console.log(
+            `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`,
+          );
+      } finally {
+        await client.close();
+      }
+    }
+    run().catch(console.dir);
 }
 
 module.exports = { upload : uploadImg };
