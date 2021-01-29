6032757581ae686a5e097ef5e32c97740a07c5f8
Kah Ho <sweekahho@gmail.com>
2021-01-29 09:41:30 +0800

    Create responsive bar chart in staff statistics

--- public/js/statistics.js
+++ public/js/statistics.js
@@ -1,15 +1,40 @@
-//alert(data);
-var trace1 = {
-    x: [1, 2, 3, 4],
-    y: [10, 15, 13, 17],
-    type: 'scatter'
+var entryArray = [0,0,0,0,0,0,0,0,0,0,0,0];
+var exitArray = [0,0,0,0,0,0,0,0,0,0,0,0];
+var hours = ['07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00']
+
+for (i in data){
+    hour = data[i].entry[0].timeString.substring(0,2);
+    index = 0;
+    if (hour <= 7)
+        index = 0;
+    else if (hour >= 18)
+        index = 11;
+    else{
+        for (j = 1; j < 11; j++){
+            if (hour == j + 7)
+                index = j;
+        }
+    }
+    if (data[i].entry[0].type == "Entry")
+        entryArray[index] += 1;
+    else
+        exitArray[index] += 1;
+}
+
+var bar1 = {
+    x: hours,
+    y: entryArray,
+    name: 'Entry',
+    type: 'bar'
 };
-  
-var trace2 = {
-    x: [1, 2, 3, 4],
-    y: [16, 5, 11, 9],
-    type: 'scatter'
+
+var bar2 = {
+    x: hours,
+    y: exitArray,
+    name: 'Exit',
+    type: 'bar'
 };
-  
-var plotdata = [trace1, trace2];
-Plotly.newPlot('plot', plotdata);
+
+var plotdata = [bar1, bar2];
+var layout = {barmode: 'stack'};
+Plotly.newPlot('plot', plotdata, layout);
