fa2ce7e98a9cd488ee87ad65abe8a42804e8a72f
noderous <s10185333@connect.np.edu.sg>
2021-02-02 00:29:58 +0800

    Download data from staff verify page

--- controllers/DAL.js
+++ controllers/DAL.js
@@ -12,6 +12,9 @@ function uploadImg(studentNo, image){
         const filter = { "_id": studentNo };
         const options = { upsert: true };
         const updateDoc = {
+          $set: {
+            "verified": false
+          },
           $push: {
             "Images": image
           }
@@ -30,5 +33,39 @@ function uploadImg(studentNo, image){
     }
     run().catch(console.dir);
 }
+
+function verified(studentNo){
+  var MongoClient = require('mongodb').MongoClient;
+    var url = "mongodb+srv://dbUser:Asteria987@cluster0.plmvw.mongodb.net/Cluster0?retryWrites=true&w=majority";
+
+    const client = new MongoClient(url);
+    async function run() {
+      try {
+        await client.connect();
+        const database = client.db("Sentry");
+        const collection = database.collection("Images");
+        const filter = { "_id": studentNo };
+        const options = { upsert: true };
+        const updateDoc = {
+          $set: {
+            "verified": true
+          },
+        };
+        
+        const result = await collection.updateOne(filter, updateDoc, options);
+        if (result.matchedCount == 0)
+          console.log("A document is created");
+        else
+          console.log(
+            `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`,
+          );
+      } finally {
+        await client.close();
+      }
+    }
+    run().catch(console.dir);
+}
+
+
 
 module.exports = { upload : uploadImg };
--- controllers/staff.js
+++ controllers/staff.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
 var middleware = require('./middleware');
 
@@ -28,6 +29,37 @@ router.get('/statistics', function (req, res){
             var data = result; 
             db.close();
             res.render('staffStatistics', {raw: data});
+        });
+    });
+})
+
+router.get('/verify', function (req, res){
+    var i = 0; // used for while loop
+    var img = [];
+
+    /*import package to connect to db*/
+    var MongoClient = require('mongodb').MongoClient;
+    /*connection string to connect to db */
+    var url = "mongodb+srv://dbUser:Asteria987@cluster0.plmvw.mongodb.net/Cluster0?retryWrites=true&w=majority";
+
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err; 
+        var dbo = db.db("Sentry"); 
+        dbo.collection("Images").find({}).toArray(function(err, result) {
+            if (err) throw err; 
+            var r = result; 
+            db.close(); 
+    
+            while (i < r.length)
+            {
+                console.log(r[i]["_id"]);
+                if (r[i]["verified"] == false)
+                {
+                    img.push({"_id": r[i]["_id"], "image": r[i]["Images"][r[i]["Images"].length - 1]});
+                }
+                i++;
+            }
+            res.render('staffVerify', {data:img});
         });
     });
 })
